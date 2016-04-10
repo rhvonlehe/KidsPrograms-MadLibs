45b64b23ec8ccbae9b27a8c75c45da017c4b7db6
Bennett <Bennett>
2016-04-10 18:26:43 -0500

    Added nouns and verbs

--- MadLibs.py
+++ MadLibs.py
@@ -13,6 +13,24 @@ def random_adjective():
 
     return random.choice(adjectives)
 
+def random_noun():
+    nouns = ['rock', 'man', 'car', 'woman', 'house', 'cabin',
+             'windows', 'river', 'mouse', 'dog', 'computer',
+             'cat', 'bigfoot', 'alien', 'balloon', 'circus',
+             'piano', 'plant', 'whale', 'telephone pole',
+             'fish', 'gremlin', 'bus', 'light switch', 'mole',
+             'pimple', 'booger', 'fingernail', 'paint can' ]
+
+    return random.choice(nouns)
+
+def random_verbs_present_participle():
+    verbs = ['running', 'walking', 'spitting', 'selling', 'buying',
+             'reading', 'farting', 'herding', 'brushing', 'jumping', 'riding',
+             'yelling', 'playing', 'talking', 'driving', 'writing', 'calling',
+             'swimming', 'sleeping', 'feeding', 'eating', 'jumping', 'losing',
+             'wandering', 'exploring']
+
+    return random.choice(verbs)
 
 def mad_libs(argv):
     while True:
@@ -21,10 +39,14 @@ def mad_libs(argv):
         if username == 'quit':
             break
 
-        print "You are very " + random_adjective() + ", " + username
+        print username + " was " + random_verbs_present_participle() + \
+            " when all of a sudden there appeared a " + \
+            random_adjective() + " " + random_noun() + "."
 
+        #print "You are very " + random_adjective() + ", " + username
 
 if __name__ == "__main__":
     random.seed(datetime.now())
 
     mad_libs(sys.argv[1:])
+
